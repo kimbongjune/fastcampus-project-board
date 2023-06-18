c0719e54bd74f88defcb5357e2f67bd1ed1a7b6e
김봉준 <nocdu112@naver.com>
2023-06-18 23:42:19 +0900

    # 16 - 게시판 뷰 기본 틀 작성
    게시글 전체조회 컨트롤러 테스트가 통과하게 변경하였음
    뷰 html 페이지의 틀을 구현하였음
    테스트에서 contentType 은 url 인코딩이 붙은 미디어 타입을 체크할 수 없어서 contentTypeCompatibleWith 로 변경하였음

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
     @DisplayName("[View][GET] 게시글 리스트 페이지 정상호출")
     @Test
     void givenNothing_whenRequestArticlesView_thenReturnArticlesView() throws Exception {
@@ -32,7 +31,7 @@ class ArticleControllerTest {
         //Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
